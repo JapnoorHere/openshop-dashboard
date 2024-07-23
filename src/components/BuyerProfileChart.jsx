c8f55d0b5eed4e8cd5758e936c44b395e8d4a55d
Japnoor Singh <sjapnoor11@gmail.com>
2024-07-23 10:00:00 +0530

    buyer profile

--- src/components/BuyerProfileChart.jsx
+++ src/components/BuyerProfileChart.jsx
@@ -10,17 +10,7 @@ const data = [
 const RADIAN = Math.PI / 180
 const COLORS = ['#00C49F', '#FFBB28', '#FF8042']
 
-const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent }) => {
-	const radius = innerRadius + (outerRadius - innerRadius) * 0.5
-	const x = cx + radius * Math.cos(-midAngle * RADIAN)
-	const y = cy + radius * Math.sin(-midAngle * RADIAN)
 
-	return (
-		<text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
-			{`${(percent * 100).toFixed(0)}%`}
-		</text>
-	)
-}
 
 export default function BuyerProfileChart() {
 	return (
